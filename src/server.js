6ab84cb36acae75a02ed7ec56978d40043894118
Sahil Kamila <sahilkamila143@gmail.com>
2026-01-29 10:55:01 +0530

    refactor: improve server startup order and async handling

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import { connectRedis } from "./config/redis.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-connectDB()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
-})
+const startServer = async () => {
+  try {
+    await connectDB();
 
-.catch((err) => {
-    console.log("MongoDB connection error ", err);
-})
+    await connectRedis();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
